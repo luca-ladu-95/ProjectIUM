10445387cb55af7107bc2dce494fe0dec2a029ed
ladul <@luca-ladu-95>
2020-02-08 14:55:52 +0100

    risolto i problemi delle mappe

---
+++ app/src/main/java/com/example/projectium/GeocodingLocation.java
@@ -0,0 +1,47 @@
+package com.example.projectium;
+
+import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+public class GeocodingLocation {
+
+    private static final String TAG = "GeocodingLocation";
+
+    public static LatLng getAddressFromLocation(final String locationAddress,
+                                                final Context context) {
+
+
+        Geocoder geocoder = new Geocoder(context, Locale.getDefault());
+        List<Address> address;
+        LatLng p1 = null;
+
+        try {
+            address = geocoder.getFromLocationName(locationAddress, 5);
+            if (address == null) {
+                return null;
+            }
+            Address location = address.get(0);
+            location.getLatitude();
+            location.getLongitude();
+
+            p1 = new LatLng(location.getLatitude(), location.getLongitude());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return p1;
+
+
+    }
+
+}
