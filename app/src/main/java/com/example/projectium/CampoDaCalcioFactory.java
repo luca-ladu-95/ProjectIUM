76fa88430ba194f63b6e57fee679bf818c039c0d
dshot92 <dshot92@gmail.com>
2020-02-12 15:29:11 +0100

    no Cap in CampoDaCalcio Factory

--- app/src/main/java/com/example/projectium/CampoDaCalcioFactory.java
+++ app/src/main/java/com/example/projectium/CampoDaCalcioFactory.java
@@ -40,14 +40,14 @@ public class CampoDaCalcioFactory {
     public void setCampiDefault (HashSet<CampoDaCalcio> set){
 
         if(set == null || set.isEmpty()){
-            set.add(creaCampo("Bonaria","2.00",10,"Via Ravenna, 09125 Cagliari","3456282932","Erba",39.209651,9.126720));
-            set.add(creaCampo("Ossigeno","2,50",10,"Viale Cimitero, 29, 09125 Cagliari","3478293914","Sintetico",39.212714,9.124624));
-            set.add(creaCampo("Terrapieno","1,50",10,"Viale Regina Elena, 14, 09124 Cagliari","3456282932","Sintetico",39.219123,9.117656));
-            set.add(creaCampo("Salesiani","2,00",10," Via Sant'Ignazio da Laconi, 09123 Cagliari","3456282932","Erba", 39.221463,9.109041));
-            set.add(creaCampo("Argonne","2,00",10," Via Argonne, 09123 Cagliari","3456282932","Erba", 39.233013,9.105147));
-            set.add(creaCampo("Montesanto","2,00",10," Via Montesanto, 09122 Cagliari","3456282932","Erba", 39.236863,9.102035));
-            set.add(creaCampo("Biasi","2,00",10," Via Biasi, 09123 Cagliari","3456282932","Erba", 39.233054,9.123792));
-            set.add(creaCampo("Piazza Giovanni","2,00",10," Piazza Giovanni, 09122 Cagliari","3456282932","Erba",39.228087,9.125191));
+            set.add(creaCampo("Bonaria","2.00",10,"Via Ravenna, Cagliari","3456282932","Erba",39.209651,9.126720));
+            set.add(creaCampo("Ossigeno","2,50",10,"Viale Cimitero, 29, Cagliari","3478293914","Sintetico",39.212714,9.124624));
+            set.add(creaCampo("Terrapieno","1,50",10,"Viale Regina Elena, 14, Cagliari","3456282932","Sintetico",39.219123,9.117656));
+            set.add(creaCampo("Salesiani","2,00",10," Via Sant'Ignazio da Laconi, Cagliari","3456282932","Erba", 39.221463,9.109041));
+            set.add(creaCampo("Argonne","2,00",10," Via Argonne, Cagliari","3456282932","Erba", 39.233013,9.105147));
+            set.add(creaCampo("Montesanto","2,00",10," Via Montesanto, Cagliari","3456282932","Erba", 39.236863,9.102035));
+            set.add(creaCampo("Biasi","2,00",10," Via Biasi, Cagliari","3456282932","Erba", 39.233054,9.123792));
+            set.add(creaCampo("Piazza Giovanni","2,00",10," Piazza Giovanni, Cagliari","3456282932","Erba",39.228087,9.125191));
         }
 
     }
@@ -55,14 +55,14 @@ public class CampoDaCalcioFactory {
     public ArrayList<CampoDaCalcio> getCampiDefault(){
 
         ArrayList<CampoDaCalcio> def = new ArrayList<>();
-        def.add(creaCampo("Bonaria","2.00",10,"Via Ravenna, 09125 Cagliari","3456282932","Erba",39.209651,9.126720));
-        def.add(creaCampo("Ossigeno","2,50",10,"Viale Cimitero, 29, 09125 Cagliari","3478293914","Sintetico",39.212714,9.124624));
-        def.add(creaCampo("Terrapieno","1,50",10,"Viale Regina Elena, 14, 09124 Cagliari","3456282932","Sintetico",39.219123,9.117656));
-        def.add(creaCampo("Salesiani","2,00",10," Via Sant'Ignazio da Laconi, 09123 Cagliari","3456282932","Erba", 39.221463,9.109041));
-        def.add(creaCampo("Argonne","2,00",10," Via Argonne, 09123 Cagliari","3456282932","Erba",39.233013,9.105147));
-        def.add(creaCampo("Montesanto","2,00",10," Via Montesanto, 09122 Cagliari","3456282932","Erba", 39.236863,9.102035));
-        def.add(creaCampo("Biasi","2,00",10," Via Biasi, 09123 Cagliari","3456282932","Erba", 39.233054,9.123792));
-        def.add(creaCampo("Piazza Giovanni","2,00",10," Piazza Giovanni, 09122 Cagliari","3456282932","Erba", 39.228087,9.125191));
+        def.add(creaCampo("Bonaria","2.00",10,"Via Ravenna, Cagliari","3456282932","Erba",39.209651,9.126720));
+        def.add(creaCampo("Ossigeno","2,50",10,"Viale Cimitero, 29, Cagliari","3478293914","Sintetico",39.212714,9.124624));
+        def.add(creaCampo("Terrapieno","1,50",10,"Viale Regina Elena, 14, Cagliari","3456282932","Sintetico",39.219123,9.117656));
+        def.add(creaCampo("Salesiani","2,00",10," Via Sant'Ignazio da Laconi, Cagliari","3456282932","Erba", 39.221463,9.109041));
+        def.add(creaCampo("Argonne","2,00",10," Via Argonne, Cagliari","3456282932","Erba",39.233013,9.105147));
+        def.add(creaCampo("Montesanto","2,00",10," Via Montesanto, Cagliari","3456282932","Erba", 39.236863,9.102035));
+        def.add(creaCampo("Biasi","2,00",10," Via Biasi, Cagliari","3456282932","Erba", 39.233054,9.123792));
+        def.add(creaCampo("Piazza Giovanni","2,00",10," Piazza Giovanni, Cagliari","3456282932","Erba", 39.228087,9.125191));
 
         return def;
 
