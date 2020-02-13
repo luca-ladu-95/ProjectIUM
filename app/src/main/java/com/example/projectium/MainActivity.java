461ff78a220b127cc58c94e786bc5e27750663c3
dshot92 <dshot92@gmail.com>
2020-02-13 09:08:25 +0100

    tolti cap e sistemati info campo da mappa

--- app/src/main/java/com/example/projectium/MainActivity.java
+++ app/src/main/java/com/example/projectium/MainActivity.java
@@ -16,7 +16,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static int SPLASH_SCREEN = 100;
+    private static int SPLASH_SCREEN = 1000;
 
     //Variabili
     Animation topAnim, bottomAnim;
