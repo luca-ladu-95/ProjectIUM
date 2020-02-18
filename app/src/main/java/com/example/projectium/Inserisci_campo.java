b84c3bcb87fd60c57d6981de80337fc605a571e0
Daniele Stochino <girasole4ever@tiscali.it>
2020-02-18 11:58:03 +0100

    Sistemato Inserisci_Campo

--- app/src/main/java/com/example/projectium/Inserisci_campo.java
+++ app/src/main/java/com/example/projectium/Inserisci_campo.java
@@ -35,8 +35,9 @@ public class Inserisci_campo extends AppCompatActivity {
     SeekBar seekBarValutazione;
     TextView valoreValutazione;
     CampoDaCalcio campo;
-    String prezzoCampo;     //Prezzo finale dopo i controlli
-    String prezzoProv;      //Prezzo provvisorio per i controlli
+    String prezzoCampo;     //Prezzo finale dopo i controlli;
+    String telProv;         //Numero di telefono provvisorio dopo i controlli
+    String telFin;          //Numero di telefono dopo i controlli
 
     int MinValue=0;
     int maxValue=5;
@@ -52,7 +53,7 @@ public class Inserisci_campo extends AppCompatActivity {
         Window window = Inserisci_campo.this.getWindow();
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
         window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-        window.setStatusBarColor(ContextCompat.getColor(Inserisci_campo.this, R.color.colorPrimaryDarkStatusBar));
+        window.setStatusBarColor(ContextCompat.getColor(Inserisci_campo.this, R.color.colorPrimaryDark));
         // infe colore della status bar
 
         nomeCampo = findViewById(R.id.gestore_nome_campo);
@@ -67,6 +68,7 @@ public class Inserisci_campo extends AppCompatActivity {
         indietro=findViewById(R.id.button_return_gestore_inserisci_campo);
 
 
+
         Intent intent = getIntent();
         Serializable obj = intent.getSerializableExtra(PERSON_DA_PASSARE);
 
@@ -78,7 +80,7 @@ public class Inserisci_campo extends AppCompatActivity {
         conferma.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(!checkInput()){
+                if(!checkInput() && !checkTel(telefono.getEditText().getText().toString())){
 
                     GeocodingLocation locationAddress = new GeocodingLocation();
                     String via = viaCampo.getEditText().getText().toString();
@@ -88,46 +90,37 @@ public class Inserisci_campo extends AppCompatActivity {
 
                     //CONTROLLI IMPORTO CAMPO
                     prezzoCampo = importoCampo.getEditText().getText().toString();
-                    String prezzoFin = prezzoCampo;
-
-
-                    if(prezzoCampo.length() < 2)    //Il prezzo inserito ha una sola cifra(ES "2")
-                        prezzoFin = prezzoCampo.concat(".00");
-                    else{
-                        //La stringa dell'importo del campo viene copiata in un array di caratteri per scomporla
-                        final char[] pr = new char[prezzoCampo.length()];
-                        prezzoCampo.getChars(0, (prezzoCampo.length()), pr, 0);
-                        prezzoCampo = String.valueOf(pr);
-
-                        for(int i = 0; i < prezzoCampo.length(); i++){
-                            if(String.valueOf(pr[i]).equals(".")){      //Nell'importo è stato inserito il punto
-                                if(i + 1 == prezzoCampo.length())   //Nell'importo inserito, non è presente nessuna cifra dopo il punto
-                                    prezzoFin = prezzoCampo.concat("00");
-                                else{
-                                    if((prezzoCampo.length() - i) == 3){    //L'importo inserito rispetta i controlli
-                                        prezzoFin = prezzoCampo;
-                                        i = prezzoCampo.length();
-                                    }else if((prezzoCampo.length() - i) == 2){  //L'importo inserito ha solo una cifra dopo il punto
-                                        prezzoFin = prezzoCampo.concat("0");
-                                    }else{      //L'importo inserito ha più di 2 cifre dopo il punto
-                                        prezzoProv = String.valueOf(pr[0]);
-                                        prezzoFin = prezzoProv.concat(".").concat(String.valueOf(pr[i + 1]).concat(String.valueOf(pr[i + 2])));
-                                        i = prezzoCampo.length();
-                                    }
-                                }
-                            }else if(i + 1 == prezzoCampo.length()){
-                                prezzoFin = prezzoCampo.concat(".00");
-                            }
-                        }
-                    }
+                    prezzoCampo = checkImport(prezzoCampo);
                     //FINE CONTROLLI IMPORTO CAMPO
 
 
+                    //INIZIO CONTROLLI NUMERO DI TELEFONO
+                    telProv = telefono.getEditText().getText().toString();
+                    telFin = telProv;
+
+                    //La stringa del numero di telefono viene copiata in un array di caratteri per scomporla
+                    final char[] t = new char[telProv.length()];
+                    telProv.getChars(0, (telProv.length()), t, 0);
+                    if(telProv.length() == 9){  //Il numero inserito ha 9 cifre (NUMERO FISSO)
+                        if(String.valueOf(t[0]).concat(String.valueOf(t[1])).concat(String.valueOf(t[2])).equals("070")){   //Il numero inserito è valido (ES: 070 92 12 30)
+                            telFin = telProv;
+                        }
+                    }else if(telProv.length() == 10){     //Il numero inserito ha 10 cifre (NUMERO DEL CELLULARE SENZA PREFISSO +39)
+                        if(!String.valueOf(t[0]).concat(String.valueOf(t[1])).concat(String.valueOf(t[2])).equals("+39")){       //Il numero inserito è valido e viene aggiunto il prefisso
+                            telFin = "+39".concat(telProv);
+                        }
+                    }else if(telProv.length() == 13){   //Il numero inserito ha 13 cifre (NUMERO DEL CELLULARE CON PREFISSO +39)
+                        if(String.valueOf(t[0]).concat(String.valueOf(t[1])).concat(String.valueOf(t[2])).equals("+39"))    //Il numero inserito è valido
+                            telFin = telProv;
+                    }
+                    //FINE CONTROLLI NUMERO DI TELEFONO
+
+                    //PASSAGGIO DEI DATI PER L'INSERIMENTO DEL NUOVO CAMPO
                     campo = CampoDaCalcioFactory.getInstance().creaCampo(
                             nomeCampo.getEditText().getText().toString(),
-                            prezzoFin,
+                            prezzoCampo,
                             10,viaCampo.getEditText().getText().toString(),
-                            telefono.getEditText().getText().toString(),
+                            telFin,
                             materiale.getEditText().getText().toString(),
                             coordinate.latitude,
                             coordinate.longitude
@@ -203,7 +196,6 @@ public class Inserisci_campo extends AppCompatActivity {
         newVal = newVal > maxValue ? maxValue : newVal;
 
         newVal = newVal < MinValue ? MinValue : newVal;
-
         this.modValue = newVal;
 
         valoreValutazione.setText(""+this.modValue);
@@ -257,6 +249,94 @@ public class Inserisci_campo extends AppCompatActivity {
 
     }
 
+    public String checkImport(String prezzo){
+
+        String prezzoFin = prezzo;
+        String prezzoProv = "";
+
+        if(prezzo.length() < 2)    //Il prezzo inserito ha una sola cifra(ES "2")
+            return prezzo.concat(".00");
+        else{
+            //La stringa dell'importo del campo viene copiata in un array di caratteri per scomporla
+            final char[] pr = new char[prezzo.length()];
+            prezzo.getChars(0, (prezzo.length()), pr, 0);
+            prezzo = String.valueOf(pr);
+
+            for(int i = 0; i < prezzo.length(); i++){
+                if(String.valueOf(pr[i]).equals(".")){      //Nell'importo è stato inserito il punto
+                    if(i + 1 == prezzo.length())   //Nell'importo inserito, non è presente nessuna cifra dopo il punto
+                        return prezzo.concat("00");
+                    else{
+                        if((prezzo.length() - i) == 3){    //L'importo inserito rispetta i controlli
+                            return prezzo;
+
+                        }else if((prezzo.length() - i) == 2){  //L'importo inserito ha solo una cifra dopo il punto
+                            return prezzo.concat("0");
+                        }else{      //L'importo inserito ha più di 2 cifre dopo il punto
+                            prezzoProv = String.valueOf(pr[0]);
+                            return prezzoProv.concat(".").concat(String.valueOf(pr[i + 1]).concat(String.valueOf(pr[i + 2])));
+
+                        }
+                    }
+                }else if(i + 1 == prezzo.length()){
+                    return prezzo.concat(".00");
+                }
+            }
+        }
+
+        return prezzoFin;
+    }
+
+    public boolean checkTel(String tel){
+
+        String telef = tel;      //Numero finale
+        boolean flag = false;
+
+        //La stringa del numero di telefono viene copiata in un array di caratteri per scomporla
+        final char[] t = new char[tel.length()];
+        tel.getChars(0, (tel.length()), t, 0);
+        if(tel.length() == 9){  //Il numero inserito ha 9 cifre (NUMERO FISSO)
+            if(String.valueOf(t[0]).concat(String.valueOf(t[1])).concat(String.valueOf(t[2])).equals("070")){   //Il numero inserito è valido (ES: 070 92 12 30)
+                telefono.setError(null);
+            }else{
+                telefono.setError("Inserisci un numero di telefono valido");    //Il numero inserito non è valido
+                flag = true;
+            }
+        }else if(tel.length() == 10){     //Il numero inserito ha 10 cifre (NUMERO DEL CELLULARE SENZA PREFISSO +39)
+            if(String.valueOf(t[0]).concat(String.valueOf(t[1])).concat(String.valueOf(t[2])).equals("+39")) {    //Il numero inserito non è valido
+                telefono.setError("Inserisci un numero di telefono valido");
+                flag = true;
+            }else{       //Il numero inserito è valido e viene aggiunto il prefisso
+                telefono.setError(null);
+            }
+        }else if(tel.length() == 13){   //Il numero inserito ha 13 cifre (NUMERO DEL CELLULARE CON PREFISSO +39)
+            if(String.valueOf(t[0]).concat(String.valueOf(t[1])).concat(String.valueOf(t[2])).equals("+39"))    //Il numero inserito è valido
+                telefono.setError(null);
+            else{       //Il numero inserito non è valido
+                telefono.setError("Inserisci un numero di telefono valido");
+                flag = true;
+            }
+        }else{
+            //Il numero inserito non è valido
+            telefono.setError("Inserisci un numero di telefono valido");
+            flag = true;
+        }
+
+        return flag;
+    }
+
+    public boolean checkCoordinates(LatLng coordinate){
+        boolean flag = false;
+
+        if(coordinate == null){
+            viaCampo.setError("Indirizzo non valido!");
+            flag = true;
+        }else
+            viaCampo.setError(null);
+
+        return flag;
+    }
+
     public void onBackPressed(){
         Intent showHOME = new Intent(Inserisci_campo.this, Gestore.class);
         //Inserisco la persona dentro l'intent
@@ -267,5 +347,4 @@ public class Inserisci_campo extends AppCompatActivity {
         startActivity(showHOME);
         finish();
     }
-
 }
